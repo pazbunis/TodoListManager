a99708920526c1b7212375b2c5a9830c5d172d8a
Paz Bunis <pazbunis@gmail.com>
2015-03-14 13:59:32 +0200

    working version

--- app/src/main/java/il/ac/huji/todolistmanager/TodoListManagerActivity.java
+++ app/src/main/java/il/ac/huji/todolistmanager/TodoListManagerActivity.java
@@ -1,27 +1,47 @@
 package il.ac.huji.todolistmanager;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.ListView;
 
 import java.util.ArrayList;
 
 
 public class TodoListManagerActivity extends ActionBarActivity {
-    public ArrayList<String> listItems = new ArrayList<String>();
-
+    public ArrayList<String> listItems = new ArrayList<>();
+    public myCustomAdapter<String> todoListAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list_manager);
         ListView lstTodoItems = (ListView)findViewById(R.id.lstTodoItems);
-        ArrayAdapter<String> todoListAdapter = new ArrayAdapter<String>(
+        todoListAdapter = new myCustomAdapter<>(
                 this, android.R.layout.simple_list_item_1, listItems);
         lstTodoItems.setAdapter(todoListAdapter);
-
+        lstTodoItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, final int position, long id) {
+                String task = listItems.get(position);
+                AlertDialog.Builder builder = new AlertDialog.Builder(TodoListManagerActivity.this);
+                builder.setTitle(task)
+                       .setNegativeButton(R.string.delete, new DialogInterface.OnClickListener() {
+                           public void onClick(DialogInterface dialog, int id) {
+                               listItems.remove(position);
+                               todoListAdapter.notifyDataSetChanged();
+                           }
+                       });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+                return false;
+            }
+        });
 
 
     }
@@ -43,11 +63,15 @@ public class TodoListManagerActivity extends ActionBarActivity {
 
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menuItemAdd) {
+            EditText edtNewItem = (EditText)findViewById(R.id.edtNewItem);
+            listItems.add(edtNewItem.getText().toString());
+            edtNewItem.setText("");
+            todoListAdapter.notifyDataSetChanged();
             return true;
-
         }
 
         return super.onOptionsItemSelected(item);
     }
+
 }
